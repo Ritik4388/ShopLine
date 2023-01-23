f9505617a99e1dbb1e2966876cb1f12a13aded95
unknown <ritikbharti4388@gmail.com>
2023-01-23 16:59:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/homePage/home";
+import Navbar from "./components/navbar/navbar";
+import Category from "./components/category/category";
+import ProductScreen from "./components/productScreen/productScreen";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route exact path="/men" element={<Category category="men's clothing"/>} />
+        <Route exact path="/women" element={<Category category="women's clothing"/>} />
+        <Route exact path="/electronics" element={<Category category="electronics"/>} />
+        <Route exact path="/jewelery" element={<Category category="jewelery"/>} />
+        <Route exact path="/products/:id" element={<ProductScreen />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
